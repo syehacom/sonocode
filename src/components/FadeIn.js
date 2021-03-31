70691768521a4defb1f79e6f0001243675a34436
syehacom <kenshicaster@gmail.com>
2021-04-01 00:25:51 +0900

    master

--- src/components/FadeIn.js
+++ src/components/FadeIn.js
@@ -2,25 +2,25 @@ import React from "react"
 import styled from "styled-components"
 import InViewMonitor from "react-inview-monitor"
 
-export const Up = ({ children, delay, margin = "-30%" }) => (
+export const Up = ({ children, delay, margin = "-25%" }) => (
   <InViewMonitor childPropsInView={{ isActive: true }} intoViewMargin={margin}>
     <FadeInUp isActive={false} delay={delay}>{children}</FadeInUp>
   </InViewMonitor>
 )
 
-export const Down = ({ children, delay, margin = "-30%" }) => (
+export const Down = ({ children, delay, margin = "-25%" }) => (
   <InViewMonitor childPropsInView={{ isActive: true }} intoViewMargin={margin}>
     <FadeInDown isActive={false} delay={delay}>{children}</FadeInDown>
   </InViewMonitor>
 )
 
-export const Left = ({ children, delay, margin = "-30%" }) => (
+export const Left = ({ children, delay, margin = "-25%" }) => (
   <InViewMonitor childPropsInView={{ isActive: true }} intoViewMargin={margin}>
     <FadeInLeft isActive={false} delay={delay}>{children}</FadeInLeft>
   </InViewMonitor>
 )
 
-export const Right = ({ children, delay, margin = "-30%" }) => (
+export const Right = ({ children, delay, margin = "-25%" }) => (
   <InViewMonitor childPropsInView={{ isActive: true }} intoViewMargin={margin}>
     <FadeInRight isActive={false} delay={delay}>{children}</FadeInRight>
   </InViewMonitor>
